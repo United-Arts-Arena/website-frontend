78ac42eb25bfb35678210f051c0a010af8613f74
Karsten Buckstegge <k.buckstegge@gmail.com>
2021-11-29 17:00:24 +0100

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Home } from "./pages/Home/Home";
+import { NotFound } from "./pages/NotFound/NotFound";
+import { Impressum } from "./pages/Impressum/Impressum";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="impressum" element={<Impressum />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Impressum/Impressum.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Impressum: React.FC = () => (
+  <main>This is where the impressum goes...</main>
+);
