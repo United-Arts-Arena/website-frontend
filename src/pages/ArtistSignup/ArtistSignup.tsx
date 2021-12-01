8c04158f5ecadeaa5d41548f383779ea8971f8cc
Karsten Buckstegge <k.buckstegge@gmail.com>
2021-12-01 12:45:04 +0100

    feat: artist signup via guestoo

--- src/pages/ArtistSignup/ArtistSignup.tsx
+++ src/pages/ArtistSignup/ArtistSignup.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
+import styles from "./artistSignup.module.scss";
+
 export const ArtistSignup: React.FC = () => (
-  <span>Artists sign up here...</span>
+  <span>
+    <div id="guestoo-registerForm"></div>
+    <iframe
+      className={styles.iframe}
+      title="guestoo_frame"
+      src="https://app.guestoo.de/public/event/4ec66dd8-b206-4358-8a03-638ead270c63/frame?hideCookieinfo=true&lang=de&scrollId=guestoo-registerForm"
+      id="guestooFrame"
+    >
+      {" "}
+    </iframe>
+    <script
+      type="text/javascript"
+      src="https://app.guestoo.de/frame-support.js"
+    ></script>
+  </span>
 );
